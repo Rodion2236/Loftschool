ec6e9bb760e47fce5a1bfa2dc3ff089a1791f546
Rodion <kosolapkinr@gmail.com>
2022-04-28 20:34:57 +0400

    add RecyclerView, DividerItemDecoration and Answer Result

--- app/src/main/java/com/rodion2236/loftmoney/AdditemActivity.kt
+++ app/src/main/java/com/rodion2236/loftmoney/AdditemActivity.kt
@@ -2,10 +2,25 @@ package com.rodion2236.loftmoney
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rodion2236.loftmoney.databinding.ActivityAdditemBinding
+import com.rodion2236.loftmoney.databinding.ActivityMainBinding
 
 class AdditemActivity : AppCompatActivity() {
+
+    lateinit var bindingClass: ActivityAdditemBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_additem)
+        setContentView(bindingClass.root)
+    }
+
+    fun AddNewTitle(view: View) {
+
+        intent.putExtra("key" , bindingClass.title.text.toString())
+        intent.putExtra("key" , bindingClass.cost.text.toString())
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
