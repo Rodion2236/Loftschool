eacbd2115edafab7745bedefac09b0c4c0f15c74
Rodion <kosolapkinr@gmail.com>
2022-05-15 19:52:29 +0400

    LoftApp is no longer an abstract class

--- app/src/main/java/com/rodion2236/loftmoney/LoftApp.kt
+++ app/src/main/java/com/rodion2236/loftmoney/LoftApp.kt
@@ -8,9 +8,8 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
-abstract class LoftApp: Application(), AuthApi {
+class LoftApp: Application() {
 
     lateinit var moneyApi: MoneyApi
     lateinit var authApi: AuthApi
